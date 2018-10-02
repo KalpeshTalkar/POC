910a62f85c088a7db578c16e3e8b0f3831bfc452
Kalpesh Talkar <kalpeshtalkar@gmail.com>
2018-10-02 18:14:42 +0530

    initial commit

---
+++ POC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  POC
+//
+//  Created by Kalpesh Talkar on 02/10/18.
+//  Copyright © 2018 Kalpesh Talkar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
